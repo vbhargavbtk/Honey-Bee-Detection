99bd4e02e04b29986a945e01530a89a976434404
vbhargavbtk <vbhargavbtk@gmail.com>
2025-12-30 23:51:53 +0530

    Stop tracking run_locally.bat, .gitkeep and media files

--- run_locally.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Bee Detection Web App...
-cd /d "%~dp0"
-.\yolovenv\Scripts\python.exe -m uvicorn app:app --reload
-if %errorlevel% neq 0 (
-    echo.
-    echo Server stopped or crashed.
-    pause
-)
